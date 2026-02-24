5c0d86593f3a9dd360e830c3a2ab9638ce27d599
Kollur-albert02 <kolluralbert@gmail.com>
2026-02-24 15:01:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,11 +86,11 @@ def book_appointment():
         otp = random.randint(100000, 999999)
         session["otp"] = str(otp)
 
-        return f"Your OTP is {otp}"
+        print("OTP:", otp)   # for testing (shows in Render logs)
 
-        #return redirect(url_for("verify_otp"))
+        return redirect(url_for("verify_otp"))
 
-    #return render_template("book.html")
+    return render_template("book.html")
 
 
 # ---------------- OTP VERIFICATION ----------------
@@ -152,5 +152,6 @@ def history():
 # ---------------- RUN SERVER ----------------
 #if __name__ == "__main__":
     #app.run(debug=True)
+
 
 
